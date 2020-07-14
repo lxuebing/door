b8dccf6fe4ff800fd2ea4aeb2def73b07b90fd78
Xuebing Li <1466371706@qq.com>
2020-07-14 22:19:48 +0800

    [xuebing] #001 feat: some test

---
+++ door/src/component/home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'red',
+    marginTop: 50
+  },
+});
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <View>
+        <Text style={styles.text}>111</Text>
+      </View>
+    )
+  }
+}
+
+export default Home;
