3ef897b4c935533ae106e8a79527fa0a2ca76459
Xuebing Li <1466371706@qq.com>
2020-07-22 21:42:23 +0800

    [xuebing] #002 feat: add bottomTab

--- door/App.js
+++ door/App.js
@@ -6,12 +6,27 @@
  * @flow strict-local
  */
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 
 import Home from './src/page/home';
+import Category from './src/page/category';
+import ShoppingCart from './src/page/shoppingCart';
+import User from './src/page/user';
 
+const Tab = createBottomTabNavigator();
 const App: () => React$Node = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="首页" component={Home} />
+        <Tab.Screen name="分类" component={Category} />
+        <Tab.Screen name="购物车" component={ShoppingCart} />
+        <Tab.Screen name="我的" component={User} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- door/src/page/home.js
+++ door/src/page/home.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import User from './user';
 
 const styles = StyleSheet.create({
   text: {
@@ -18,7 +17,6 @@ class Home extends React.Component {
     return (
       <View>
         <Text style={styles.text}>home</Text>
-        <User />
       </View>
     );
   }
