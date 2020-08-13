bc46a2b12adbf7275a3dcd048ff2de4be0817193
gavin <whq_1991@163.com>
2020-08-14 00:39:49 +0800

    登录

--- door/src/api/storage.js
+++ door/src/api/storage.js
@@ -7,11 +7,6 @@ export const storage = new Storage({
   defaultExpires: null
 });
 
-storage.save({
-  key:'token',
-  data:"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzIiwic3ViIjoidWRldiIsImlhdCI6MTU5NzI0NDkwNywiZXhwIjoxNTk3MjcwMTA3fQ.UvAHlStWCLjqLH8frauksungzgVuGaereigk1OZyuQY"
-})
-
 /**
  * 客户端缓存保存 token 数据
  * @param {string} data.access_token
